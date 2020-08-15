120ae8455e252879aa9c2cda0309e79af64d1ceb
Edmundo <sivasadartantasvueltasnosirve@gmail.com>
2020-08-15 03:07:33 -0300

    Update JUL.main.jl

--- linux/JUL.main.jl
+++ linux/JUL.main.jl
@@ -1,5 +1,7 @@
 
-	4aa5f673174155d84fcf6ca781777c620191189f
+	# !/bin/bash
+
+	/bin/svn --password 4aa5f673174155d84fcf6ca781777c620191189f commit --message "From file"
 	# Loading the functions
 
 	include("JUL.auxiliar.jl") ;
